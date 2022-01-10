896d18c8887bb186380b3685ec3b6470c8f70cd9
Steven Ritchie <steven.ritchie@natwest.com>
2022-01-10 14:01:43 +0000

    run_kubernetes.sh task

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,23 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sritchie1305/project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run project4\
+    --image=&dockerpath\
+    --port=80 --labels app=project4
+
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+sleep 60s
+kubectl get pods
+kubectl port-forward project4 8000:80
 
